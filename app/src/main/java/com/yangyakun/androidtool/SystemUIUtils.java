ee8c9097ba4290cbeb8c1fe2d6274c4dd27f4999
android <361176335@qq.com>
2018-03-02 08:56:42 +0800

    修改项目为 桌面程序 并设置全屏

---
+++ app/src/main/java/com/yangyakun/androidtool/SystemUIUtils.java
@@ -0,0 +1,21 @@
+package com.yangyakun.androidtool;
+
+import android.view.View;
+
+/**
+ * Created by android on 2018/2/28.
+ */
+
+public class SystemUIUtils {
+    public static void setStickFullScreen(View view) {
+        int systemUiVisibility = view.getSystemUiVisibility();
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        systemUiVisibility |= flags;
+        view.setSystemUiVisibility(systemUiVisibility);
+    }
+}
