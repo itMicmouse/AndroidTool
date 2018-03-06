a24e5a58f8c816cac4462fef874e3bf543a17ede
android <361176335@qq.com>
2018-03-06 10:04:33 +0800

    打开 开发助手

--- app/src/main/java/com/yangyakun/androidtool/MainActivity.java
+++ app/src/main/java/com/yangyakun/androidtool/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yangyakun.androidtool;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         SystemUIUtils.setStickFullScreen(getWindow().getDecorView());
+    }
+
+    public void opensettting(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String packageName = "cn.trinea.android.developertools";
+        String className = "cn.trinea.android.developertools.MainActivity";
+        intent.setClassName(packageName, className);
+
+
+//        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        startActivity(intent);
     }
 }
