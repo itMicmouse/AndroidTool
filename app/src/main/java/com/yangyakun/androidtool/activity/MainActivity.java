3e24ab36fcba45d6844fe71180972ef52186a1e4
android <361176335@qq.com>
2018-05-23 08:05:25 +0800

    开发准备

--- app/src/main/java/com/yangyakun/androidtool/activity/MainActivity.java
+++ app/src/main/java/com/yangyakun/androidtool/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yangyakun.androidtool.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +15,7 @@ import com.yangyakun.androidtool.R;
 
 import java.lang.reflect.Method;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/yangyakun/androidtool/adapter/RecycleViewAdapter.java
@@ -0,0 +1,60 @@
+package com.yangyakun.androidtool.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.yangyakun.androidtool.R;
+
+import java.util.ArrayList;
+
+/**
+ * @author android
+ * @date 2018/5/21.
+ */
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder> {
+    private ArrayList<String> mData;
+
+    public RecycleViewAdapter(ArrayList<String> data) {
+        this.mData = data;
+    }
+
+    public void updateData(ArrayList<String> data) {
+        this.mData = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 实例化展示的view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_rv_item, parent, false);
+        // 实例化viewholder
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // 绑定数据
+        holder.mTv.setText(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox mTv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTv = (CheckBox) itemView.findViewById(R.id.item_tv);
+        }
+    }
+
+}
