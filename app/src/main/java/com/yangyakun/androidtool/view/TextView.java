9d24eee2debb85f7a891be40655f6f7d133ce77d
yangyakun <yangyakun921@gmail.com>
2019-05-06 10:48:34 +0800

    Paint画笔修改

--- app/src/main/java/com/yangyakun/androidtool/view/TextView.java
+++ app/src/main/java/com/yangyakun/androidtool/view/TextView.java
@@ -1,39 +1,72 @@
 package com.yangyakun.androidtool.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ComposeShader;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.yangyakun.androidtool.R;
 import com.yangyakun.androidtool.utils.Utils;
+
+import java.util.logging.SocketHandler;
 
 public class TextView extends View {
 
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Path path = new Path();
+    private Paint mPaint;
+    private Shader mShader;
+    private Bitmap mBitmap;
 
     public TextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        path.reset();
-        path.addRect(getWidth()/2-150,getHeight()/2-300,getWidth()/2+150,getHeight()/2,Path.Direction.CCW);
-
+    private void init() {
+        mPaint = new Paint();
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_directory_grey600_36dp);
+        mPaint.setAntiAlias(true);//设置抗锯齿效果
+        mPaint.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawLine(100,100,200,200,paint);
+//        mShader = new LinearGradient(0,0,500,500,
+//                new int[]{Color.RED,Color.BLUE,Color.GREEN},new float[]{0.5f,0.7f,1}, Shader.TileMode.CLAMP);
+//        mPaint.setShader(mShader);
+//        canvas.drawRect(0,0,500,500,mPaint);
 
-        canvas.drawCircle(getWidth()/2,getHeight()/2,Utils.dp2px(150),paint);
+//        mShader = new RadialGradient(250,250,250,
+//                new int[]{Color.GREEN,Color.YELLOW,Color.RED},null,Shader.TileMode.CLAMP);
+//        mPaint.setShader(mShader);
+//        canvas.drawCircle(250,250,250,mPaint);
+
+//        mShader = new SweepGradient(250,250,Color.RED,Color.GREEN);
+//        mPaint.setShader(mShader);
+//        canvas.drawCircle(250,250,250,mPaint);
+
+//        mShader = new BitmapShader(mBitmap,Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+//        mPaint.setShader(mShader);
+//        canvas.drawRect(0,0,mBitmap.getWidth(),mBitmap.getHeight(),mPaint);
+
+        LinearGradient mShader1 = new LinearGradient(0,0,500,500,
+                new int[]{Color.RED,Color.BLUE,Color.GREEN},new float[]{0.5f,0.7f,1}, Shader.TileMode.CLAMP);
+        BitmapShader mShader2 = new BitmapShader(mBitmap,Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mShader = new ComposeShader(mShader1,mShader2, PorterDuff.Mode.MULTIPLY);
+        canvas.drawCircle(250,250,250,mPaint);
     }
 }
